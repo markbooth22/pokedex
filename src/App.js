e28de215525ac8ea531d78c865a5d9970bbf4337
Sterling Watts 🍷 <SterlingDWatts@gmail.com>
2020-12-22 01:46:19 -0800

    Fix API call and add deploy

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [pokedex, setPokedex] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=10000")
@@ -12,8 +13,9 @@ function App() {
       .then((res) => {
         const { results } = res;
         setPokedex(results);
+        setIsLoading(false)
       });
-  });
+  }, []);
 
   return <div className="App"></div>;
 }
